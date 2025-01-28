984d6453e19c7d9657039b4813f5a0e809be1042
Jared Palmer <jared@jaredpalmer.com>
2025-01-28 11:29:44 -0500

    feat: Add generate-query utility function

---
+++ src/generate-query.ts
@@ -0,0 +1,97 @@
+import { format } from 'date-fns/format';
+import { z } from 'zod';
+// import JSON5 from 'json5'
+import { Message } from 'ai';
+import { ChatCompletionMessageParam } from 'openai/resources';
+import { openai } from '@/lib/openai';
+
+// const listSchema = z.array(z.string()).default([])
+
+// const allowList = listSchema.parse(
+//   JSON5.parse(process.env.WEBSEARCH_ALLOWLIST!)
+// )
+// const blockList = listSchema.parse(
+//   JSON5.parse(process.env.WEBSEARCH_BLOCKLIST!)
+// )
+
+// const queryModifier = [
+//   ...allowList.map(item => 'site:' + item),
+//   ...blockList.map(item => '-site:' + item)
+// ].join(' ')
+
+export async function generateQuery(messages: Message[]) {
+  const currentDate = format(new Date(), 'MMMM d, yyyy');
+  const userMessages = messages.filter(({ role }) => role === 'user');
+  const previousUserMessages = userMessages.slice(0, -1);
+
+  const lastMessage = userMessages.slice(-1)[0];
+
+  const convQuery: Array<Omit<Message, 'id'>> = [
+    {
+      role: 'user',
+      content: `Previous Questions:
+- Who is the president of France?
+
+Current Question: What about Mexico?
+`,
+    },
+    {
+      role: 'assistant',
+      content: 'President of Mexico',
+    },
+    {
+      role: 'user',
+      content: `Previous questions: 
+- When is the next formula 1 grand prix?
+
+Current Question: Where is it being hosted ?`,
+    },
+    {
+      role: 'assistant',
+      content: 'location of next formula 1 grand prix',
+    },
+    {
+      role: 'user',
+      content:
+        'Current Question: What type of printhead does the Epson F2270 DTG printer use?',
+    },
+    {
+      role: 'assistant',
+      content: 'Epson F2270 DTG printer printhead',
+    },
+    { role: 'user', content: 'What were the news yesterday ?' },
+    {
+      role: 'assistant',
+      content: `news ${format(new Date(Date.now() - 864e5), 'MMMM d, yyyy')}`,
+    },
+    { role: 'user', content: 'What is the current weather in Paris ?' },
+    { role: 'assistant', content: `weather in Paris ${currentDate}` },
+    {
+      role: 'user',
+      content:
+        (previousUserMessages.length > 0
+          ? `Previous questions: \n${previousUserMessages
+              .map(({ content }) => `- ${content}`)
+              .join('\n')}`
+          : '') +
+        '\n\nCurrent Question:' +
+        lastMessage.content,
+    },
+  ];
+
+  const { choices } = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: `You are tasked with generating web search queries. Give me an appropriate query to answer my question for google search. Answer with only the query. Today is ${currentDate}`,
+      },
+      ...convQuery,
+    ] as ChatCompletionMessageParam[],
+  });
+
+  const webQuery = choices[0].message.content;
+
+  // return (queryModifier + ' ' + webQuery).trim()
+  return (webQuery ?? '').trim();
+}
